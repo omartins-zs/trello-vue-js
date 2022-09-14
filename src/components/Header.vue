b4896cfe3903bfeecbbb293d8aeada1138dfa9ae
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-14 15:56:43 -0300

    Criando component Header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  background-color: var(--color-blue);
+  height: 4.5rem;
+}
+</style>
