43e56e6af7791f070a58b5504ada8cab4b3fbae9
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-14 15:54:25 -0300

    Criando Conteudo dos Cards Fakes

---
+++ src/cards.js
@@ -0,0 +1,55 @@
+export default {
+  afazer: [
+    {
+      id: 1,
+      text: "Primeira Tarefa",
+    },
+    {
+      id: 2,
+      text: "Segunda Tarefa",
+    },
+
+    {
+      id: 3,
+      text: "Terceira Tarefa",
+    },
+  ],
+  fazendo: [
+    {
+      id: 1,
+      text: "Primeira Tarefa",
+    },
+  ],
+  finalizados: [
+    {
+      id: 1,
+      text: "Primeira Tarefa",
+    },
+    {
+      id: 2,
+      text: "Segunda Tarefa",
+    },
+    {
+      id: 3,
+      text: "Terceira Tarefa",
+    },
+    {
+      id: 4,
+      text: "Quarta Tarefa",
+    },
+  ],
+  observacoes: [
+    {
+      id: 1,
+      text: "Primeira Tarefa",
+    },
+    {
+      id: 2,
+      text: "Segunda Tarefa",
+    },
+    {
+      id: 3,
+      text: "Terceira Tarefa",
+    },
+  ],
+};
